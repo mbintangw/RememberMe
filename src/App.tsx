562d74e01ef48526e263f68bf4f389eba0536df6
mbintangw <mbintangw@gmail.com>
2024-07-30 14:38:30 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import MainLayout from "./Layout/MainLayout";
+import HomePages from "./Pages/HomePages";
+
+function App() {
+  return (
+    <MainLayout>
+      <HomePages/>
+    </MainLayout>
+  );
+}
+
+export default App;
