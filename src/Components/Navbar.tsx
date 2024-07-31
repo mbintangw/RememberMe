6860c53df772fe81bffb969093f3b8d871d6b912
mbintangw <mbintangw@gmail.com>
2024-07-31 13:50:46 +0700

    feat : add Navbar

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,8 +1,10 @@
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
+    <nav>
+      <div className="flex justify-center px-7 py-5 mx-auto border-b-2 border-white">
+        <h1 className="text-headline text-4xl uppercase text-transparent bg-clip-text bg-gradient-to-r from-headline to-paragraph font-bold">Remember Me</h1>
+      </div>
+    </nav>
   )
 }
 
