f68cb5409a1d387556aca51294bb4986bc862308
mbintangw <mbintangw@gmail.com>
2024-07-31 13:50:09 +0700

    core : add colors and fontfamily

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,23 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif'],
+      },
+
+      colors: {
+        'background': '#0f0e17',
+        'headline' : '#fffffe',
+        'paragraph': '#a7a9be',
+        'button' : '#ff8906',
+        'buttontext' : '#fffffe',
+        'stroke' : 'black',
+        'highlight' : '#ff8906',
+        'secondary' : '#f25f4c',
+        'tertiary' : '#e53170',
+      }
+    },
   },
   plugins: [],
 }
