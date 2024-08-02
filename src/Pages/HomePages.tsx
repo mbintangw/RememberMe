4990fb303095d48d06131d77c4cf780a0e180287
mbintangw <mbintangw@gmail.com>
2024-08-02 15:06:43 +0700

    feat : edit component homepage

--- src/Pages/HomePages.tsx
+++ src/Pages/HomePages.tsx
@@ -1,5 +1,13 @@
+import TaskForm from "../Components/TaskForm";
+import TaskList from "../Components/TaskList";
+
 const HomePages = () => {
-  return <div></div>;
+  return (
+  <div className="flex flex-col gap-10 justify-center">
+    <TaskForm/>
+    <TaskList/>
+  </div>
+  );
 };
 
 export default HomePages;
