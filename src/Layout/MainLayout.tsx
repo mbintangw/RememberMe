a664d2c09bc61d35dbe5ee08b802022bd243ba96
mbintangw <mbintangw@gmail.com>
2024-08-02 14:32:34 +0700

    style : change gap

--- src/Layout/MainLayout.tsx
+++ src/Layout/MainLayout.tsx
@@ -2,7 +2,7 @@ import Navbar from "../Components/Navbar";
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col gap-10">
       <Navbar />
       <main>{children}</main>
     </div>
