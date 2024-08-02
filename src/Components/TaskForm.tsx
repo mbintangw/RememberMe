c98589c2dc46fc89a576e2cc97dc93287ef7be62
mbintangw <mbintangw@gmail.com>
2024-08-02 15:05:10 +0700

    feat : add Task Form

---
+++ src/Components/TaskForm.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TaskForm = () => {
+  return (
+    <main className='flex justify-center items-center'>
+      <button className='bg-tertiary p-3 rounded-lg'>+ Add Task</button>      
+    </main>
+  )
+}
+
+export default TaskForm
