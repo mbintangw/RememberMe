64bff242757b92d8d33797f5e46188df7d7de2d8
mbintangw <mbintangw@gmail.com>
2024-08-02 18:28:09 +0700

    feat : add tasklist props to homepage

--- src/Pages/HomePages.tsx
+++ src/Pages/HomePages.tsx
@@ -1,13 +1,21 @@
 import TaskForm from "../Components/TaskForm";
 import TaskList from "../Components/TaskList";
+import { TbTargetArrow } from "react-icons/tb";
+import { LuCalendarClock } from "react-icons/lu";
+import { FaRegCircleCheck } from "react-icons/fa6";
 
 const HomePages = () => {
   return (
-  <div className="flex flex-col gap-10 justify-center">
-    <TaskForm/>
-    <TaskList/>
-  </div>
+    <div className="flex flex-col gap-20 justify-center">
+      <TaskForm />
+      <div className="flex flex-col lg:flex-row justify-center lg:justify-evenly items-center gap-20">
+        <TaskList title="To do" icon={<TbTargetArrow />} />
+        <TaskList title="Due Date" icon={<LuCalendarClock />} />
+        <TaskList title="Completed" icon={<FaRegCircleCheck />} />
+      </div>
+    </div>
   );
 };
+
 
 export default HomePages;
